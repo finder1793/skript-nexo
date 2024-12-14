ab78f347d9dc1ac8462b4add3d5db6b1f5de503c
finder1793 <kangas.kyle@gmail.com>
2024-12-14 17:00:43 -0600

    Version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ java {
 
 description = "A skript addon that aims to link Nexo and Skript"
 group = "me.asleepp"
-version = "1.0.5"
+version = "1.0.6"
 
 dependencies {
     compileOnly(libs.paper.api)
