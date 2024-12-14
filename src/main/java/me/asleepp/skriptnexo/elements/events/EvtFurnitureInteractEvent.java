8df9ca66a1b5baf4cdfab5adda5e6d0c4352bb88
finder1793 <kangas.kyle@gmail.com>
2024-12-14 15:04:13 -0600

    t

--- src/main/java/me/asleepp/skriptnexo/elements/events/EvtFurnitureInteractEvent.java
+++ src/main/java/me/asleepp/skriptnexo/elements/events/EvtFurnitureInteractEvent.java
@@ -47,8 +47,8 @@ public class EvtFurnitureInteractEvent extends SkriptEvent {
         }, 0);
         EventValues.registerEventValue(NexoFurnitureInteractEvent.class, BlockFace.class, new Getter<BlockFace, NexoFurnitureInteractEvent>() {
             @Override
-            public BlockFace get(NexoFurnitureInteractEvent event) {
-                return arg.getBlockFace();
+            public BlockFace get(NexoFurnitureInteractEvent arg) {
+                return arg.getblockFace();
             }
         }, 0);
     }
