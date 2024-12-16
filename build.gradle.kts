87f0bef51c1e20041a7f8046029964c8c7ef9de0
finder1793 <kangas.kyle@gmail.com>
2024-12-16 23:30:09 +0000

    1.0.8 version Bump adding (custom|nexo) block ID of %block%")

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ java {
 
 description = "A skript addon that aims to link Nexo and Skript"
 group = "me.asleepp"
-version = "1.0.7"
+version = "1.0.8"
 
 dependencies {
     compileOnly(libs.paper.api)
