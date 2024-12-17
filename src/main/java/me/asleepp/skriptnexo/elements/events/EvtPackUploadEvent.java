86afce1ec66c243d5e247f879656faa764b67563
finder1793 <kangas.kyle@gmail.com>
2024-12-17 20:35:32 +0000

    adds on pack upload

---
+++ src/main/java/me/asleepp/skriptnexo/elements/events/EvtPackUploadEvent.java
@@ -0,0 +1,54 @@
+package me.asleepp.skriptnexo.elements.events;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.Literal;
+import ch.njol.skript.lang.SkriptEvent;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.registrations.EventValues;
+import ch.njol.skript.util.Getter;
+import com.nexomc.nexo.api.events.resourcepack.NexoPackUploadEvent;
+import org.bukkit.event.Event;
+
+import javax.annotation.Nullable;
+
+@Name("On Pack Upload")
+@Description({"Fires when the resource pack is uploaded."})
+@Examples({"on pack upload:"})
+@Since("1.0")
+public class EvtPackUploadEvent extends SkriptEvent {
+
+    static {
+        Skript.registerEvent("Pack Upload", EvtPackUploadEvent.class, NexoPackUploadEvent.class, "pack upload");
+        EventValues.registerEventValue(NexoPackUploadEvent.class, String.class, new Getter<String, NexoPackUploadEvent>() {
+            @Override
+            public String get(NexoPackUploadEvent event) {
+                return event.getHash();
+            }
+        }, 0);
+        EventValues.registerEventValue(NexoPackUploadEvent.class, String.class, new Getter<String, NexoPackUploadEvent>() {
+            @Override
+            public String get(NexoPackUploadEvent event) {
+                return event.getUrl();
+            }
+        }, 0);
+    }
+
+    @Override
+    public boolean init(Literal<?>[] args, int matchedPattern, SkriptParser.ParseResult parseResult) {
+        return true;
+    }
+
+    @Override
+    public boolean check(Event e) {
+        return e instanceof NexoPackUploadEvent;
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean debug) {
+        return "pack upload";
+    }
+}
