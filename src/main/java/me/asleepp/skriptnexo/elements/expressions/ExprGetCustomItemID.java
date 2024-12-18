27cc6fc920fd34d8c4a0627f561c97e85194aca0
finder1793 <kangas.kyle@gmail.com>
2024-12-18 00:56:43 +0000

    Possiblly parse projectile ItemID

--- src/main/java/me/asleepp/skriptnexo/elements/expressions/ExprGetCustomItemID.java
+++ src/main/java/me/asleepp/skriptnexo/elements/expressions/ExprGetCustomItemID.java
@@ -1,5 +1,3 @@
-package me.asleepp.skriptnexo.elements.expressions;
-
 import ch.njol.skript.Skript;
 import ch.njol.skript.classes.Changer;
 import ch.njol.skript.doc.Description;
@@ -12,35 +10,42 @@ import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
 import com.nexomc.nexo.api.NexoItems;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.Event;
 import org.bukkit.inventory.ItemStack;
 import ch.njol.skript.lang.ExpressionType;
 
 @Name("Get Custom Item ID")
-@Description("Gets the custom item ID from an item stack.")
-@Examples({"set {_id} to custom item ID of player's tool"})
-@Since("1.0")
+@Description("Gets the custom item ID from an item stack or projectile.")
+@Examples({"set {_id} to custom item ID of player's tool", "set {_id} to custom item ID of projectile"})
+@Since("1.1")
 public class ExprGetCustomItemID extends SimpleExpression<String> {
 
     static {
-        Skript.registerExpression(ExprGetCustomItemID.class, String.class, ExpressionType.PROPERTY, "(custom|nexo) item ID of %itemstack%");
+        Skript.registerExpression(ExprGetCustomItemID.class, String.class, ExpressionType.PROPERTY, "(custom|nexo) item ID of %itemstack/projectile%");
     }
 
-    private Expression<ItemStack> itemStackExpr;
+    private Expression<?> itemExpr;
 
     @Override
     public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
-        itemStackExpr = (Expression<ItemStack>) exprs[0];
+        itemExpr = exprs[0];
         return true;
     }
 
     @Override
     protected String[] get(Event e) {
-        ItemStack itemStack = itemStackExpr.getSingle(e);
-        if (itemStack == null) {
+        Object item = itemExpr.getSingle(e);
+        if (item == null) {
             return null;
         }
-        String itemId = NexoItems.idFromItem(itemStack);
+        String itemId = null;
+        if (item instanceof ItemStack) {
+            itemId = NexoItems.idFromItem((ItemStack) item);
+        } else if (item instanceof Projectile) {
+            ItemStack itemStack = ((Projectile) item).getItem();
+            itemId = NexoItems.idFromItem(itemStack);
+        }
         return itemId != null ? new String[]{itemId} : null;
     }
 
@@ -56,7 +61,7 @@ public class ExprGetCustomItemID extends SimpleExpression<String> {
 
     @Override
     public String toString(Event e, boolean debug) {
-        return "custom item ID of " + itemStackExpr.toString(e, debug);
+        return "custom item ID of " + itemExpr.toString(e, debug);
     }
 
     @Override
